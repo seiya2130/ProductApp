cbacf01740370929ad8165fa75f01291120922af
Seiya_Satoh <satohseiya0312@gmail.com>
2022-12-18 16:23:18 +0900

    Domain追加

---
+++ Domain/Application/Product/ProductGetAllInteractor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Application.Product
+{
+    public class ProductGetAllInteractor
+    {
+
+        public void Handle()
+        {
+
+        }
+    }
+}
