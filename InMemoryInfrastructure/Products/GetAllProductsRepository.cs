5099325053f5e25c77df29ed1fb155878bf4dbdf
Seiya_Satoh <satohseiya0312@gmail.com>
2022-12-20 09:31:12 +0900

    Repository追加

---
+++ InMemoryInfrastructure/Products/GetAllProductsRepository.cs
@@ -0,0 +1,24 @@
+﻿using Application.Product.GetAllProducts;
+using Domain.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InMemoryInfrastructure.Products
+{
+    public class GetAllProductsRepository : IGetAllProductsRepository
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        };
+
+        public List<Product> GetAllProducts()
+        {
+            return products.ToList();
+        }
+    }
+}
