1f1a3930ae4941553c5b211a5c6ce1edca501466
Seiya_Satoh <satohseiya0312@gmail.com>
2022-12-20 09:49:19 +0900

    Handleメソッド追加

--- Application/Products/GetAllProducts/GetAllProducts.cs
+++ Application/Products/GetAllProducts/GetAllProducts.cs
@@ -1,14 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Application.Products.GetAllProducts
 {
     public class GetAllProducts : IGetAllProducts
     {
+        private readonly IGetAllProductsRepository getAllProductsRepository;
+
+        public GetAllProducts(IGetAllProductsRepository getAllProductsRepository)
+        {
+            this.getAllProductsRepository = getAllProductsRepository;
+        }
+
         public List<GetAllProductsOutputData> Handle()
         {
-            return new List<GetAllProductsOutputData>();
+            var products = getAllProductsRepository.GetAllProducts();
+
+            var outputDataList = products.Select(x =>
+                new GetAllProductsOutputData() { Name = x.Name, Price = x.Price}
+            ).ToList();
+
+            return outputDataList;
         }
     }
 }
