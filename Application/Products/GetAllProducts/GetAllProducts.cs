e971328ad0c532d4a4d7ba7c37441e479a084a3f
Seiya_Satoh <satohseiya0312@gmail.com>
2022-12-21 22:22:45 +0900

    UseCaseとRepository更新

--- Application/Products/GetAllProducts/GetAllProducts.cs
+++ Application/Products/GetAllProducts/GetAllProducts.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Application.Products.GetAllProducts
 {
     public class GetAllProducts : IGetAllProducts
     {
+        private readonly IGetAllProductsRepository getAllProductsRepository;
+
+        public GetAllProducts(IGetAllProductsRepository getAllProductsRepository)
+        {
+            this.getAllProductsRepository = getAllProductsRepository;
+        }
+
         public List<GetAllProductsOutputData> Handle()
         {
-            return new List<GetAllProductsOutputData>();
+            return this.getAllProductsRepository.GetAllProducts().Select(x => new GetAllProductsOutputData(x.Name, x.Price)).ToList();
         }
     }
 }
